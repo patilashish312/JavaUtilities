1ecb218dab5cd03f7d680a4875419543e91e37de
patilashish312 <35179165+patilashish312@users.noreply.github.com>
2021-07-22 16:33:30 +0100

    Additional files addition
    Most of these solutions have been addressed via one liner stream solution,

---
+++ Solution1.java
@@ -0,0 +1,53 @@
+package app.comp;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result4 {
+	
+	//https://www.hackerrank.com/challenges/sock-merchant/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=warmup
+	/*
+	 * Complete the 'sockMerchant' function below.
+	 *
+	 * The function is expected to return an INTEGER. The function accepts following
+	 * parameters: 1. INTEGER n 2. INTEGER_ARRAY ar
+	 */
+
+	public static int sockMerchant(int n, List<Integer> ar) {
+		return ar.stream().collect(Collectors.groupingBy(x -> x)).values().stream().reduce(0, (x, y) -> {
+			x = y.size() / 2 + x;
+			return x;
+		}, (x, y) -> x = y);
+	}
+
+}
+
+public class Solution1 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+		// BufferedWriter bufferedWriter = new BufferedWriter(new
+		// FileWriter(System.getenv("OUTPUT_PATH")));
+
+		int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+		List<Integer> ar = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+				.map(Integer::parseInt).collect(toList());
+
+		int result = Result4.sockMerchant(n, ar);
+
+		// bufferedWriter.write(String.valueOf(result));
+		// bufferedWriter.newLine();
+
+		bufferedReader.close();
+		//bufferedWriter.close();
+	}
+}
